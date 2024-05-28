c96979007e596e57b59426b828024d229ad7c7a8
Vitalii-250885 <provitaleks@gmail.com>
2024-05-28 17:57:14 +0300

    123

--- src/scripts/removeAllContacts.js
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs/promises';
-
-import { PATH_DB } from '../constants/contacts.js';
-
-export const removeAllContacts = async () => {
-  await fs.writeFile(PATH_DB, '[]', 'utf-8');
-};
-
-await removeAllContacts();
