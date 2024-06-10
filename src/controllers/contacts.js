b16db51b2ab40cb65183b6aeaf57c58259d1ba52
Vitalii-250885 <provitaleks@gmail.com>
2024-06-10 12:37:18 +0300

    add sort

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -6,12 +6,22 @@ import {
   updateContact,
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
 
 export const getAllContactsController = async (req, res) => {
-  const contacts = await getAllContacts();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+
+  const contacts = await getAllContacts({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+  });
 
   res.status(200).json({
-    status: '200 OK',
+    status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
   });
@@ -28,7 +38,7 @@ export const getContactByIdController = async (req, res, next) => {
   }
 
   res.status(200).json({
-    status: '200 OK',
+    status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
   });
--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -2,8 +2,16 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  phoneNumber: Joi.number().integer().min(10).max(10).required(),
-  email: Joi.string().email(),
+  phoneNumber: Joi.number().integer().required(),
+  email: Joi.string().email().min(3).max(20),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
+});
+
+export const updateContactSchema = Joi.object({
+  name: Joi.string().min(3).max(20),
+  phoneNumber: Joi.number().integer().min(10).max(10),
+  email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().valid('work', 'home', 'personal'),
 });
